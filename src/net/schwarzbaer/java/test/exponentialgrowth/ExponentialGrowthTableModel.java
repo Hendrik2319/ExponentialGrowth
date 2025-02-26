00047ee5b41e5fe6111f3c8d1c9eeb2992623e8b
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2025-02-26 19:14:18 +0100

    feat: display of ratio value changed

--- src/net/schwarzbaer/java/test/exponentialgrowth/ExponentialGrowthTableModel.java
+++ src/net/schwarzbaer/java/test/exponentialgrowth/ExponentialGrowthTableModel.java
@@ -10,7 +10,7 @@ import net.schwarzbaer.java.lib.gui.Tables;
 
 class ExponentialGrowthTableModel extends Tables.SimpleGetValueTableModel<TableEntry, ExponentialGrowthTableModel.ColumnID>
 {
-	// Column Widths: [30, 100, 30, 100, 30, 100] in ModelOrder
+	// Column Widths: [30, 100, 40, 100, 40, 70] in ModelOrder
 	enum ColumnID implements Tables.SimpleGetValueTableModel.ColumnIDTypeInt<TableEntry>, SwingConstants
 	{
 		Index            ( config("#"                , Integer  .class,  30, CENTER).setValFunc( row -> row.index )),
@@ -18,7 +18,7 @@ class ExponentialGrowthTableModel extends Tables.SimpleGetValueTableModel<TableE
 		CurrentAmountUnit( config("Unit"             , ExpFactor.class,  40, null  ).setValFunc( row -> row.currentAmountUnit )),
 		GrowthRate_per_s ( config("Growth Rate (1/s)", Double   .class, 100, null  ).setValFunc( row -> row.growthRate_per_s ).setToString(d -> String.format(Locale.ENGLISH, "%1.3f", d))),
 		GrowthRateUnit   ( config("Unit"             , ExpFactor.class,  40, null  ).setValFunc( row -> row.growthRateUnit )),
-		Ratio            ( config("Ratio"            , Double   .class, 100, null  ).setValFunc( row -> row.ratio ).setToString(d -> String.format(Locale.ENGLISH, "%1.3E", d))),
+		Ratio            ( config("Ratio"            , Double   .class,  70, null  ).setValFunc( row -> row.ratio ).setToString(ColumnID::getRatioToString)),
 		;
 		
 		final Tables.SimplifiedColumnConfig2<ExponentialGrowthTableModel, TableEntry, ?> cfg;
@@ -29,6 +29,17 @@ class ExponentialGrowthTableModel extends Tables.SimpleGetValueTableModel<TableE
 		private static <T> Tables.SimplifiedColumnConfig2<ExponentialGrowthTableModel, TableEntry, T> config(String name, Class<T> columnClass, int prefWidth, Integer horizontalAlignment)
 		{
 			return new Tables.SimplifiedColumnConfig2<>(name, columnClass, 20, -1, prefWidth, prefWidth, horizontalAlignment);
+		}
+		
+		private static String getRatioToString(double ratio)
+		{
+			double abs = Math.abs(ratio);
+			if (abs <     10 ) return String.format(Locale.ENGLISH, "%1.5f", ratio);
+			if (abs <    100 ) return String.format(Locale.ENGLISH, "%1.4f", ratio);
+			if (abs <   1000 ) return String.format(Locale.ENGLISH, "%1.3f", ratio);
+			if (abs <  10000 ) return String.format(Locale.ENGLISH, "%1.2f", ratio);
+			if (abs < 100000 ) return String.format(Locale.ENGLISH, "%1.1f", ratio);
+			return String.format(Locale.ENGLISH, "%1.3E", ratio);
 		}
 	}
 	
